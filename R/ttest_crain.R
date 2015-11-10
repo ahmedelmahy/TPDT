4ba0363b45b65ff9aad099819b17b62e2f767c3a
Tore <tore.erdmann@gmx.de>
2015-11-10 15:34:20 +0100

    updated crainicianu pval procedure.

--- R/ttest_crain.R
+++ R/ttest_crain.R
@@ -1,36 +1,12 @@
-#' Test for group difference in functional data.
-#' @description Uses joint confidance bands for the difference curves of two groups.  
-#' Adapted from Crainiceanu, C. (2012).
-#' @param data [vector] with the response variable, i.e. values of some blood marker
-#' @param group [vector] with grouping information. Has to be same length as data and 
-#' contain two groups.
-#' @param timepoints [vector] with timepoints of measurments of the values in data
-#' @param id [vector] with index for individuals
-#' @param points [numeric] resolution of grid when discretisizing.
-#' @param lambda [numeric] provide smoothing parameter. If NULL, an optimal choice is computed
-#'  through cross-validation.
-#' @param nboot [numeric] Number of bootstrap iterations. 
-#' @param method [choice] Currently only "non-parametric"-Version of the test available.
-#' @param ncpus [Integer] number of cores to use in parallel for bootstrap.
-#' @return List with components: \itemize{
-#' \item p: pvalue for group differences
-#' 
-#' \item fit1: fda object for group 1
-#' 
-#' \item fit2: fda object for group 2
-#' 
-#' \item lambda: smoothing parameter used for fitting.
-#' 
-#' }
-#' 
-#' @example demo/ex_ttest_crain.R
-#' @export
+library(fda)
+library(mvtnorm)
+
 ttest_crain <- function(data, group, timepoints, id, 
-                             method = c("spline", "coef", "rawdata"), 
-                        points = 201, nboot = 1000, ncpus = 1, lambda = NULL) {
- 
+                        method = c("spline", "coef", "rawdata"), 
+                        points = 201, nboot = 1000, ncpus = 1, lambda = NULL, plot = FALSE) {
+  
   if(any(missing(data), missing(group), missing(timepoints), missing(id)) ||
-       any(is.null(data), is.null(group), is.null(timepoints), is.null(id)))
+     any(is.null(data), is.null(group), is.null(timepoints), is.null(id)))
     stop("Missing inputs")
   
   method <- match.arg(method)
@@ -56,8 +32,8 @@ ttest_crain <- function(data, group, timepoints, id,
     datmat <- cbind(dat1, dat2)
     timemat <- cbind(timel1, timel2)
     
-    bas <- create.bspline.basis(range(timepoints), nbasis = length(tau), norder = 4)
-
+    bas <- fda::create.bspline.basis(range(timepoints), nbasis = length(tau), norder = 4)
+    
     # exclude individuals with first or last timepoint missing:
     if(any(c(is.na(datmat[c(1, ntp), ]), is.na(timemat[c(1, ntp), ]))))
       stop("Missing values at first or last time points not permitted.")
@@ -66,7 +42,7 @@ ttest_crain <- function(data, group, timepoints, id,
       cv_out <- cv3(y = datmat, timemat = timemat, rangevals = range(timepoints),
                     nbas = length(tau), with.na = any(is.na(datmat)))
       
-        #         cv_out <- .20015
+      #         cv_out <- .20015
       # alt:
       #     data_list <- split(data, list(data$group, data$id))
       #     suppressWarnings(data_list <- as.list(split(data, group)))
@@ -80,12 +56,12 @@ ttest_crain <- function(data, group, timepoints, id,
     else
       cv_out <- lambda
     
-    par1 <- fdPar(bas, 2, lambda = cv_out)
-    par2 <- fdPar(bas, 2, lambda = cv_out)
+    par1 <- fda::fdPar(bas, 2, lambda = cv_out)
+    par2 <- fda::fdPar(bas, 2, lambda = cv_out)
     
     sm1 <- smooth.basis.na(timel1, dat1, par1)
     sm2 <- smooth.basis.na(timel2, dat2, par2)
-     
+    
     finetimes <- seq(min(timepoints), max(timepoints), length.out = points)
     matsim <- matrix(NA, ncol = length(finetimes), nrow = (n1 * n2))
     
@@ -93,7 +69,7 @@ ttest_crain <- function(data, group, timepoints, id,
     l <- 1
     for(i in 1:n1) {
       for(j in 1:n2) {
-        matsim[l, ] <- eval.fd(finetimes, sm1[i]) - eval.fd(finetimes, sm2[j])
+        matsim[l, ] <- fda::eval.fd(finetimes, sm1[i]) - eval.fd(finetimes, sm2[j])
         l <- l + 1
       }
     }  
@@ -114,35 +90,35 @@ ttest_crain <- function(data, group, timepoints, id,
   dbar <- colMeans(matsim)
   dSig <- diag(Sig)
   
-if(method == "rawdata") {
-  data_list <- tapply(data$data, INDEX = data$id, function(x) matrix(x, ncol = 1))
-  time_list <- tapply(data$time, INDEX = data$id, function(x) x)
-  bas <- create.bspline.basis(range(timepoints), nbasis = length(timepoints), norder = 4)
-  
-  if(missing(lambda))
-    cv_out <- cv.lambda2(data_list, time_list, ncores = ncpus)
-  else
-    cv_out <- lambda
-  
-  
-  par1 <- fdPar(bas, 2, lambda = cv_out)
-  dat1 <- matrix(data[grp1, "data"], 
-                 nrow = length(tau), ncol = n1, byrow = FALSE)
-  dat2 <- matrix(data[grp2, "data"], 
-                 nrow = length(tau), ncol = n2, byrow = FALSE)
-  dbar <- smooth.basis.na(tau, dat1, par1)
-}
+  if(method == "rawdata") {
+    data_list <- tapply(data$data, INDEX = data$id, function(x) matrix(x, ncol = 1))
+    time_list <- tapply(data$time, INDEX = data$id, function(x) x)
+    bas <- fda::create.bspline.basis(range(timepoints), nbasis = length(timepoints), norder = 4)
+    
+    if(missing(lambda))
+      cv_out <- cv.lambda2(data_list, time_list, ncores = ncpus)
+    else
+      cv_out <- lambda
+    
+    
+    par1 <- fda::fdPar(bas, 2, lambda = cv_out)
+    dat1 <- matrix(data[grp1, "data"], 
+                   nrow = length(tau), ncol = n1, byrow = FALSE)
+    dat2 <- matrix(data[grp2, "data"], 
+                   nrow = length(tau), ncol = n2, byrow = FALSE)
+    dbar <- fda::smooth.basis.na(tau, dat1, par1)
+  }
   # from here: Crainiceanu test
   
   xn <- 
-  mclapply(1:nboot, function(b) {
-    
-    # step1
-    dn <- rmvnorm(1, dbar, Sig, method = "svd")
-    # step2
-    max(abs(dn - dbar)/sqrt(dSig))  
-    
-  }, mc.cores = ncpus)
+    mclapply(1:nboot, function(b) {
+      
+      # step1
+      dn <- mvtnorm::rmvnorm(1, dbar, Sig, method = "svd")
+      # step2
+      max(abs(dn - dbar)/sqrt(dSig))  
+      
+    }, mc.cores = ncpus)
   
   # step3
   q1a <- quantile(unlist(xn), .95)
@@ -151,9 +127,17 @@ if(method == "rawdata") {
   confu <- dbar + q1a*sqrt(dSig)
   confl <- dbar - q1a*sqrt(dSig)
   
-  
+  # proportion of time, that CI does not include 0
   signitime <- mean(confu < 0 | confl > 0)
-  p <- 1 - signitime
+  
+  p <- 1
+  if(signitime > 0)
+    p <- .05
+  
+  p <- p * (1 - signitime) 
+  
+  #before
+  # p <- 1 - signitime
   
   # plot result
   if(method == "spline") {
@@ -161,22 +145,24 @@ if(method == "rawdata") {
   } else {
     ind <- (finetimes %% length(dbar)) %% 1 == 0
   }
-
-#   pdf("simulation_plot.pdf")
-# #   pdf(paste0("Figures/Diffplot_", unique(group)[1], "_", unique(group)[2], ".pdf"))
-#   plot(finetimes[ind], dbar, ylim = range(c(confu, confl)), t = "l", xlab = "time",
-#        ylab = "difference in both groups", 
-#        main = paste0(unique(group)[1], " vs. ", unique(group)[2], 
-#                      " \n significant diffs at 5 % level: ", 100*round(signitime, 4), 
-#                      "% of the time scale"))
-#   polygon(c(finetimes[ind], rev(finetimes[ind])), c(confu, rev(confl)), col = "#33330022")
-#   abline(h = 0)
-#   dev.off()
+  
+  if(plot) {
+    #   pdf("simulation_plot.pdf")
+    # #   pdf(paste0("Figures/Diffplot_", unique(group)[1], "_", unique(group)[2], ".pdf"))
+    plot(finetimes[ind], dbar, ylim = range(c(confu, confl)), t = "l", xlab = "time",
+         ylab = "difference in both groups", 
+         main = paste0(
+           "significant diffs at 5 % level: ", 100*round(signitime, 4), 
+           "% \n of the time scale"))
+    polygon(c(finetimes[ind], rev(finetimes[ind])), c(confu, rev(confl)), col = "#33330022")
+    abline(h = 0)
+    #   dev.off()
+  }
   
   
   # save results
-#   save.image(file = paste0("Rdata/Workspace_", var, "_", unique(group)[1], "_", 
-#                            unique(group)[2], ".Rdata"))  
+  #   save.image(file = paste0("Rdata/Workspace_", var, "_", unique(group)[1], "_", 
+  #                            unique(group)[2], ".Rdata"))  
   list("pval" = p, "lambda" = cv_out, "fit1" = sm1, "fit2" = sm2)
 }
 
